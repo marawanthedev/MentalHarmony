d9c6e67d7e2b1b502024adcaaef7cab60f96e52d
marawanthedev <tmarawan11@gmail.com>
2022-12-14 22:12:44 +0800

    fixing student dashboard bug, reverseing relative to absolute paths for imports

--- src/pages/student/student.tsx
+++ src/pages/student/student.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Templatev2 from "../../components/templatev2/templatev2";
-import BookingIcon from "../../assets/images/booking.webp";
-import ViewBookingHistory from "../../container/viewBookingHistory/viewBookingHistory";
-import Protected from "../../util/protected";
-import { tab } from "../../constants/tab";
+import Templatev2 from "components/templatev2/templatev2";
+import BookingIcon from "assets/images/booking.webp";
+import ViewBookingHistory from "container/viewBookingHistory/viewBookingHistory";
+import Protected from "util/protected";
+import { tab } from "constants/tab";
 
 export default function student() {
   const tabs: Array<tab> = [
     {
       text: "View Booking History",
       icon: BookingIcon,
-      getTabComponentRender: () => ViewBookingHistory,
+      getTabComponentRender: ViewBookingHistory,
     },
   ];
 
